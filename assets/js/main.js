3121bd158c5623c70cff5c79521c9d36b4c33b97
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-02-11 16:18:54 +0530

    REMOVE MENU MOBILE done

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,19 +17,26 @@ if(navToggle){
 
 
 /*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
 if(navClose){
     navClose.addEventListener('click',()=>{
         navMenu.classList.remove('show-menu');
     })
 }
-/* Validate if constant exists */
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
-
 
 /*==================== QUALIFICATION TABS ====================*/
 
